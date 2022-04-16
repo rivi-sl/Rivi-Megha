e0a90dc3dcfa02e4fd1a49515ce14fa4a88165be
STEdirisuriya <supunedirisuriya2002@gmail.com>
2022-04-16 16:09:30 +0530

    Changed Theme Icons

--- src/Components/Icons/Rivicon.js
+++ src/Components/Icons/Rivicon.js
@@ -7,7 +7,7 @@ const Rivicon = (props) => {
     const {i,s,mt,pt,pb,pr,pl,selected, nav, style} = props
 
     return (
-        <div className={nav ? selected ? styles.icon_selected :styles.icon : null} style={{marginTop: mt, height: s,display:'grid',paddingLeft:pl,paddingRight:pr}}>
+        <div className={nav ? selected ? styles.icon_selected + ' selected-icon' :styles.icon + ' not-selected-icon': null} style={{marginTop: mt, height: s,display:'grid',paddingLeft:pl,paddingRight:pr}}>
             {IconExplorer(s,i,style)}
         </div>
     )
--- src/Layout/Navigation.js
+++ src/Layout/Navigation.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import *  as styles from '../Styles/Navigation.module.css'
+import '../Components/Icons/Icons.module.css'
 import Rivicon from '../Components/Icons/Rivicon'
 
 const Navigation = () => {
     return (
         <div className={styles.navigationBar}>
             <section className={styles.profile}>
-                <Rivicon i='ProfileIcon' s='32' mt={0} selected={false} nav={true} /> 
+                <Rivicon i='ProfileStyleIcon' s='32' mt={0} selected={false} nav={true}/> 
             </section>
             <section className={styles.pages}>
-                <Rivicon i='GlobalIcon' s='32' mt={30} selected={false} nav={true} />
-                <Rivicon i='PrivateIcon' s='32' mt={30} selected={true} nav={true} /> 
-                <Rivicon i='GroupIcon' s='32' mt={30} selected={false} nav={true} />  
+                <Rivicon i='GlobalStyleIcon' s='32' mt={30} selected={false} nav={true}/>
+                <Rivicon i='PrivateStyleIcon' s='32' mt={30} selected={true} nav={true}/> 
+                <Rivicon i='GroupStyleIcon' s='32' mt={30} selected={false} nav={true}/>  
             </section>
             <section className={styles.settings}>
-                <Rivicon i='SettingsIcon' s='32' mt={0} selected={false} nav={true} />
+                <Rivicon i='SettingsStyleIcon' s='32' mt={0} selected={false} nav={true}/>
             </section>
 
         </div>
