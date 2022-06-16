20785e01c33ea6d6a388d816f373292ab0f5a22f
NisanduAthsara <nisandunox@gmail.com>
2022-06-16 17:22:58 +0530

    Make some changes

--- src/utilities/Auth.Context.js
+++ src/utilities/Auth.Context.js
@@ -12,6 +12,7 @@ export const AuthProvider = ({children}) => {
     const [isLogged,setIsLogged] = useState(false)
     const [currentUser, setCurrentUser] = useState()
     const [loading,setLoading] = useState(true)
+    const [token,setToken] = useState('')
 
     function login(email,password) {
         setLoading(true)
@@ -28,9 +29,9 @@ export const AuthProvider = ({children}) => {
 		axios.post('https://rivi-test-backend.herokuapp.com/api/v1/user/signin', reqObject, axiosConfig)
 			.then((res) => {
 				if(res.data.success === true){
-					setIsLogged(true)
-                    setCurrentUser(res.data)
-                    console.log(res.data)
+                    setToken(res.data.token)
+					// setIsLogged(true)
+                    // setCurrentUser(res.data)
 				}else if(res.data.message === 'Invalid email'){
 					//make the err message
 					alert('Invalid email')		
@@ -43,7 +44,21 @@ export const AuthProvider = ({children}) => {
 			.catch((err) => {
 				console.log("AXIOS ERROR: ", err);
 			})
-            setLoading(false)
+
+        axios.get(`https://rivi-test-backend.herokuapp.com/api/v1/user/getUser?token=${token}`, axiosConfig)
+            .then((res) => {
+                if(res.data.success === false){
+                    alert(res.data.message)
+                }else{
+                    setCurrentUser(res.data.user)
+                    setIsLogged(true)
+                }
+            })
+            .catch((err) => {
+                console.log("AXIOS ERROR: ", err);
+            })    
+
+        setLoading(false)
     }
 
     const signup = (username,email,password,mobile) => {
@@ -60,19 +75,20 @@ export const AuthProvider = ({children}) => {
 			}
 		};
 
+        //https://rivi-test-backend.herokuapp.com/api/v1/user/signup
 		axios.post('https://rivi-test-backend.herokuapp.com/api/v1/user/signup', reqObj, axiosConfig)
 			.then((res) => {
-				if(res.data.success === false){
-                    alert(res.data.message)
+				if(res.success === false){
+                    alert(res.message)
                 }else{
                     // alert(res.data.message)
-                    setIsLogged(true)
-                    setCurrentUser(res.data.user)
                 }
 			})
 			.catch((err) => {
 				console.log("AXIOS ERROR: ", err);
 			})
+        
+        
     }
 
     const logout = () => {
